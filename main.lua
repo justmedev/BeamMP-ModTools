926e1e575256c3bcb307d72fc5a354b21ec5736e
Ilja Busch <ilja.ragnar@gmail.com>
2022-01-14 18:41:19 +0100

    fixed function order

--- main.lua
+++ main.lua
@@ -34,6 +34,22 @@ local commands = {
     ["usage"]="player_name",
   },
 }
+
+function LoadBanListFile ()
+  local jsonFile = io.open("banlist.json", "rb")
+
+  if jsonFile == nil then
+    print(banlistName .. " is missing. Creating one for you...")
+    io.open(banlistName, "w"):write("{ banned: [] }"):close()
+    jsonFile = io.open(banlistName, "rb")
+  end
+
+  local content = jsonFile:read("a")
+  jsonFile:close()
+
+  return json.parse(content)
+end
+
 local banlist = LoadBanListFile()
 
 -- Registering events
@@ -43,7 +59,7 @@ MP.RegisterEvent("onPlayerJoin", "OnPlayerJoin")
 -- Events
 function OnPlayerJoin (player_id)
   local payer_name = MP.GetPlayerName(player_id)
-  
+
   if ArrayContains(banlist.banned, payer_name) then
     print("Banned player '" ..  MP.GetPlayerName(player_id) .. "' Just tried to join.")
     MP.DropPlayer(player_id, "You are banned from this server.")
@@ -55,99 +71,99 @@ function HandleChatMessage(sender_id, sender_name, message)
   if message:sub(1,1) ~= prefix then return 0 end
   local split = SplitString(message:sub(2), " ")
   local cmd = split[1]
-  
+
   local args = {}
   if #split > 1 then
     table.remove(split, 1)
     args = split
   end
-  
+
   local id = MP.GetPlayerIdentifiers(sender_id)
   if logToConsole then print("Player " .. sender_name .. "(" .. sender_id .. ";" .. id.beammp .. ") Just executed the command '" .. prefix .. cmd .. "'.") end
-  
+
   -- check if player has rights to execute the command; if not: show help
   if ArrayContains(admins, id.beammp) == false then
     CommandNotFound(sender_id, cmd)
     return 1
   end
-  
+
   if cmd == "kick" then
     local reason = "No reason specified"
-    if #args < 1 then 
+    if #args < 1 then
       MP.SendChatMessage(sender_id, "Required argument player_name missing!")
       return 1
     end
-    
+
     if #args > 1 then reason = table.concat(args:sub(2), " ") end
-    
+
     local playerId = FindPlayerByName(args[1])
-    
+
     if playerId == -1 then
       MP.SendChatMessage(sender_id, "Player '" .. args[1] .. "' not found!")
       return 1
     end
-    
+
     MP.DropPlayer(playerId, reason)
     return 1
-    
+
   elseif cmd == "ban" then
     local reason = "No reason specified."
-    if #args < 1 then 
+    if #args < 1 then
       MP.SendChatMessage(sender_id, "Required argument player_name missing!")
       return 1
     end
-    
+
     if #args > 1 then reason = table.concat(args:sub(2), " ") end
-    
+
     local playerId = FindPlayerByName(args[1])
-    
+
     if playerId == nil then
       MP.SendChatMessage(sender_id, "Player '" .. args[1] .. "' not found!")
       return 1
     end
-    
+
     MP.DropPlayer(playerId, banMessage .. reason)
-    
+
     table.insert(banlist.banned, args[1])
     SaveBanListFile()
     return 1
-    
+
   elseif cmd == "unban" then
-    if #args < 1 then 
+    if #args < 1 then
       MP.SendChatMessage(sender_id, "Required argument player_name missing!")
       return 1
     end
-    
+
     for i, n in ipairs(banlist.banned) do
       if args[1] == n then table.remove(banlist, i) end
     end
-    
+
     SaveBanListFile()
     MP.SendChatMessage("Unbanned player '" .. args[1] .. "'.")
     return 1
-    
+
   elseif cmd == "list" then
     MP.SendChatMessage(sender_id, "------ [Online Players] ------")
-    
+
     for id, name in pairs(MP.GetPlayers()) do
       MP.SendChatMessage(sender_id, id .. ": " .. name)
     end
-    
+
     return 1
-    
+
   elseif cmd == "help" then
     MP.SendChatMessage(sender_id, "------ [ModTools Help] ------")
     MP.SendChatMessage(sender_id, "Note: [...] is optional")
-    
+
     for _, command in ipairs(commands) do
       local usage = ""
       if command.usage then usage = " " .. command.usage end
-      
+
       local msg = prefix .. command.name .. usage .. ": " .. command.description
       MP.SendChatMessage(sender_id, msg)
     end
     return 1
-    
+
   else
     -- no command found
     CommandNotFound(sender_id, cmd)
@@ -165,23 +181,8 @@ function FindPlayerByName (name)
       return i
     end
   end
-  
-  return nil
-end
 
-function LoadBanListFile ()
-  local jsonFile = io.open("banlist.json", "rb")
-  
-  if jsonFile == nil then
-    print(banlistName .. " is missing. Creating one for you...")
-    io.open(banlistName, "w"):write("{ banned: [] }"):close()
-    jsonFile = io.open(banlistName, "rb")
-  end
-  
-  local content = jsonFile:read("a")
-  jsonFile:close()
-  
-  return json.parse(content)
+  return nil
 end
 
 function SaveBanListFile ()
